d61f780a7338ecaa665a4a619009d74c52e201fa
suwan <skdoqj@naver.com>
2023-10-17 10:15:51 +0900

    10.7

---
+++ Documents/CODE/threejs/learnThreejs/js/app.js
@@ -0,0 +1,35 @@
+import * as THREE from "../node_modules/three/build/three.module.js";
+console.log(THREE);
+
+const scene = new THREE.Scene();
+
+//카메라
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  1,
+  500
+);
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
+
+//렌더러
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+//create a blue LineBasicMaterial
+const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+
+const points = [];
+points.push(new THREE.Vector3(-10, 0, 0));
+points.push(new THREE.Vector3(0, 10, 0));
+points.push(new THREE.Vector3(10, 0, 0));
+
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+const line = new THREE.Line(geometry, material);
+
+scene.add(line);
+renderer.render(scene, camera);
